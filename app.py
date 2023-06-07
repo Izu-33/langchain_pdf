1f1847d18f2017272d271f2442e75ff8b55503e0
Izundu Dan-Ekeh <dizundu@yahoo.com>
2023-06-07 06:35:30 +0100

    Updated AI app with langchain features

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
 
 def main():
     load_dotenv()
@@ -7,6 +15,44 @@ def main():
     st.header("Ask about your PDF")
 
     pdf = st.file_uploader('Upload your PDF', type='pdf')
+
+    if pdf:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # Split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text)
+        
+        # Create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # Collect user input
+        user_question = st.text_input("Ask a question about your PDF")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+            # View results of similarity search
+            # st.write(docs)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type='stuff')
+            response = chain.run(input_documents=docs, question=user_question)
+
+            st.write(response)
+
+        
+        # Write out chunks
+        # st.write(chunks)
 
 if __name__ == "__main__":
     main()
